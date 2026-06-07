b432690723e98ff903d6829ffafba8113f81d140
Satyamkumar Singh <satyamranjeet2006@gmail.com>
2026-06-07 06:59:26 +0530

    Update title and navbar branding to use full name SKRS for consistency

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -43,7 +43,7 @@ const Navbar = () => {
     <>
       <div className="header">
         <a href="/#" className="navbar-title" data-cursor="disable">
-          SS
+          SKRS
         </a>
         <a
           href="mailto:satyamranjeet2006@gmail.com"
